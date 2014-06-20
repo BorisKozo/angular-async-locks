18974a04734838c5b086cc4cc369c7149c8a2839
Boris Kozorovitzky <majorzbzzn@gmail.com>
2014-06-20 23:46:24 +0200

    Added unit tests for all the methods

--- app/lib/async-locks.js
+++ app/lib/async-locks.js
@@ -1,128 +1,133 @@
 angular.module('boriskozo.async-locks', [])
     .factory('AsyncLockFactory', function () {
-        'use strict';
+      'use strict';
 
-        var tokenId = 0;
+      var tokenId = 0;
 
-        function elapsed() {
-            return new Date() - this.start;
+      function elapsed() {
+        return new Date() - this.start;
+      }
+
+      /**
+       * An asynchronous lock.
+       * @constructor
+       */
+      var AsyncLock = function () {
+        this.queue = [];
+        this.ownerTokenId = null;
+      };
+
+      /**
+       * A function that is used to create a token. Override if needed.
+       * @param {function} callback - The callback associated with the acquiring of this token.
+       */
+      AsyncLock.prototype.createToken = function (callback) {
+        return {
+          id: tokenId++,
+          isCanceled: false,
+          callback: callback,
+          elapsed: elapsed,
+          start: new Date()
+        };
+      };
+
+      /**
+       * A function that is used to execute the user callback. Default implementation invokes the callback asynchronously.
+       * Override if needed.
+       * @param {object} token - The the token which contains the callback to call.
+       */
+      AsyncLock.prototype.executeCallback = function (token) {
+        setTimeout(function () { //this is because < IE10 doesn't support setTimeout with parameters
+          token.callback(token);
+        }, 0);
+      };
+
+      /**
+       * Locks the lock and generates a token which can be used to control the lock.
+       * @param {function} callback - The callback which is going to be called when the lock is acquired
+       * The callback is of the form foo(token) (i.e. it will receive the acquired token as a parameter when called)
+       * @returns The token which controls the lock for this callback.
+       */
+      AsyncLock.prototype.enter = function (callback) {
+        if (!angular.isFunction(callback)) {
+          throw new Error('Callback must be a function');
+
+        }
+        var token = this.createToken(callback);
+
+        if (token === null || token === undefined) {
+          throw new Error('Token cannot be null or undefined');
         }
 
-        /**
-         * An asynchronous lock.
-         * @constructor
-         */
-        var AsyncLock = function () {
-            this.queue = [];
-            this.ownerToken = null;
-        };
+        if (this.ownerTokenId !== null) {
+          this.queue.push(token);
+        } else {
+          this.ownerTokenId = token.id;
+          this.executeCallback(token);
+        }
+        return token;
+      };
 
-        /**
-         * A function that is used to create a token. Override if needed.
-         * @param {function} callback - The callback assosiated with the aquring of this token.
-         */
-        AsyncLock.prototype.createToken = function (callback) {
-            return {
-                id: tokenId++,
-                isCanceled: false,
-                callback: callback,
-                elapsed: elapsed,
-                start: new Date()
-            };
-        };
+      /**
+       * Releases the lock and resumes the next waiting callback.
+       * @param {object} token - The token which has acquired the lock. 
+       * This token is used only to make sure that only the appropriate owner releases the lock.
+       */
 
-        /**
-         * A function that is used to execute the user callback. Default implementation invokes the callback asynchronously.
-         * Override if needed.
-         * @param {object} token - The the token which contains the callback to call.
-         */
-        AsyncLock.prototype.executeCallback = function (token) {
-            setTimeout(function () { //this is because < IE10 doesn't support setTimeout with parameters
-                token.callback(token);
-            }, 0);
-        };
+      AsyncLock.prototype.leave = function (token) {
+        if (token === null || token === undefined) {
+          throw new Error('Token cannot be null or undefined');
+        }
+        if (this.ownerTokenId === null) {
+          throw new Error('There is no pending token in the lock but received ' + JSON.stringify(token));
+        }
 
-        /**
-         * Locks the lock and generates a token which can be used to control the lock.
-         * @param {function} callback - The callback which is going to be called when the lock is aquired
-         * The callback is of the form foo(token) (i.e. it will recieve the aquired token as a parameter when called)
-         * @returns The token which controls the lock for this callback.
-         */
-        AsyncLock.prototype.enter = function (callback) {
-            var token = this.createToken(callback);
+        if (this.ownerTokenId !== token.id) {
+          throw new Error('Owner token mismatch. Expected ' + this.ownerTokenId + ' but received ' + JSON.stringify(token.id));
+        }
 
-            if (token === null || token === undefined) {
-                throw new Error("Token cannot be null or undefined");
-            }
+        var queueToken;
+        this.ownerTokenId = null;
+        while (this.queue.length > 0) {
+          queueToken = this.queue.shift();
+          if (queueToken.isCanceled) {
+            continue;
+          }
+          this.ownerTokenId = queueToken.id;
+          this.executeCallback(queueToken);
+        }
+      };
 
-            if (this.ownerToken !== null) {
-                this.queue.push(token);
-            } else {
-                this.ownerToken = token;
-                this.executeCallback(token);
-            }
-            return token;
-        };
+      /**
+       * Releases the lock and aborts all pending callbacks on this lock.
+       * @param {object} token - The token which has acquired the lock. 
+       * This token is used only to make sure that only the appropriate owner releases the lock.
+       */
+      AsyncLock.prototype.stop = function (token) {
+        if (token === null || token === undefined) {
+          throw new Error('Token cannot be null or undefined');
+        }
+        if (this.ownerTokenId === null) {
+          throw new Error('There is no pending token in the lock but received ' + JSON.stringify(token));
+        }
 
-        /**
-         * Releases the lock and resumes the next waiting callback.
-         * @param {object} token - The token which has aquired the lock. This token is used only to make sure that only the appropriate owner releases the lock.
-         */
+        if (this.ownerTokenId !== token.id) {
+          throw new Error('Owner token mismatch. Expected ' + this.ownerTokenId + ' but received ' + JSON.stringify(token.id));
+        }
 
-        AsyncLock.prototype.leave = function (token) {
-            if (token === null || token === undefined) {
-                throw new Error("Token cannot be null or undefined");
-            }
-            if (this.ownerToken === null) {
-                throw new Error("There is no pending token in the lock but receieved " + JSON.stringify(token));
-            }
+        this.queue = [];
+        this.ownerTokenId = null;
+      };
 
-            if (this.ownerToken === null || this.ownerToken.id !== token.id) {
-                throw new Error("Owner token mismatch. Expected " + this.ownerToken + " but received " + JSON.stringify(token));
-            }
-
-            var queueToken;
-            this.ownerToken = null;
-            while (this.queue.length > 0) {
-                queueToken = this.queue.shift();
-                if (queueToken.isCanceled) {
-                    continue;
-                }
-                this.ownerToken = queueToken;
-                this.executeCallback(queueToken);
-            }
-        };
-
-        /**
-         * Releases the lock and aborts all pending callbacks on this lock.
-         * @param {object} token - The token which has aquired the lock. This token is used only to make sure that only the appropriate owner releases the lock.
-         */
-
-        AsyncLock.prototype.stop = function (token) {
-            if (token === null || token === undefined) {
-                throw new Error("Token cannot be null or undefined");
-            }
-            if (this.ownerToken === null) {
-                throw new Error("There is no pending token in the lock but receieved " + JSON.stringify(token));
-            }
-
-            if (this.ownerToken === null || this.ownerToken.id !== token.id) {
-                throw new Error("Owner token mismatch. Expected " + this.ownerToken + " but received " + JSON.stringify(token));
-            }
-
-            this.queue = [];
-            this.ownerToken = null;
-        };
-
-        return AsyncLock;
+      return AsyncLock;
 
     })
     .service('AsyncLocksService', [
 
     function () {
-            'use strict';
+      'use strict';
 
-            this.print = function () {
-                console.log('Hello World');
-            };
-}]);
+      this.print = function () {
+        console.log('Hello World');
+      };
+    }]);
--- test/unit/async-lock-spec.js
+++ test/unit/async-lock-spec.js
@@ -1,107 +1,283 @@
 describe('Async Locks', function () {
-    'use strict';
-    var expect = chai.expect;
+  'use strict';
+  var expect = chai.expect;
 
-    describe('Async Locks Service', function () {
+  describe('Async Locks Service', function () {
 
-        //        beforeEach(module('myApp.controllers'));
-        //
-        //
-        //        it('should ....', inject(function ($controller) {
-        //            //spec body
-        //            var myCtrl1 = $controller('MyCtrl1', {
-        //                $scope: {}
-        //            });
-        //            expect(myCtrl1).toBeDefined();
-        //        }));
-        //
-        //        it('should ....', inject(function ($controller) {
-        //            //spec body
-        //            var myCtrl2 = $controller('MyCtrl2', {
-        //                $scope: {}
-        //            });
-        //            expect(myCtrl2).toBeDefined();
-        //        }));
+  });
+
+  describe('Async Lock', function () {
+
+    var AsyncLock;
+
+    beforeEach(function () {
+      module('boriskozo.async-locks');
+      inject(function ($injector) {
+        AsyncLock = $injector.get('AsyncLockFactory');
+      });
     });
 
-    describe('Async Lock', function () {
+    describe('Real examples', function () {
+      function delay(balance, callback) {
+        setTimeout(function () {
+          callback(balance);
+        }, 10);
+      }
 
-        var AsyncLock;
+      it('should calculate incorrect balance without the lock', function (done) {
+        var count = 0;
+        var balance = { //Simulate a value stored on a remote server
+          value: 100
+        };
 
-        beforeEach(function () {
-            module('boriskozo.async-locks');
-            inject(function ($injector) {
-                AsyncLock = $injector.get('AsyncLockFactory');
+        function updateBalance() {
+          delay(balance.value, function (value) { //simulate reading the balance from remote server
+            value -= 10;
+            delay(value, function (value) { // simulate writing the balance back to remove server
+              balance.value = value;
+              if (count === 0) {
+                count++;
+                return;
+              }
+              expect(balance.value).to.be.equal(90);
+              done();
             });
-        });
-
-        function delay(balance, callback) {
-            setTimeout(function () {
-                callback(balance);
-            }, 10);
+          });
         }
 
+        updateBalance();
+        updateBalance();
+      });
 
+      it('should calculate correct balance with the lock', function (done) {
+        var count = 0;
+        var balance = {
+          value: 100
+        };
 
-        it('should calculate incorrect balance without the lock', function (done) {
-            var count = 0;
-            var balance = { //Simulate a value stored on a remote server
-                value: 100
-            };
+        var lock = new AsyncLock();
 
-            function updateBalance() {
-                delay(balance.value, function (value) { //simulate reading the balance from remote server
-                    value -= 10;
-                    delay(value, function (value) { // simulate writing the balance back to remove server
-                        balance.value = value;
-                        if (count === 0) {
-                            count++;
-                            return;
-                        }
-                        expect(balance.value).to.be.equal(90);
-                        done();
-                    });
-                });
-            }
+        function updateBalance() {
+          lock.enter(function (token) {
+            delay(balance.value, function (value) {
+              value -= 10;
+              delay(value, function (value) {
+                balance.value = value;
+                if (count === 0) {
+                  count++;
+                  lock.leave(token);
+                  return;
+                }
+                expect(balance.value).to.be.equal(80);
+                done();
+              });
+            });
+          });
+        }
 
-            updateBalance();
-            updateBalance();
-        });
+        updateBalance();
+        updateBalance();
+      });
+    });
 
-        it('should calculate correct balance with the lock', function (done) {
-            var count = 0;
-            var balance = {
-                value: 100
-            };
+    describe('Helper functions', function () {
+      it('should create tokens with different ids', function () {
+        var lock = new AsyncLock();
+        var token1 = lock.createToken();
+        var token2 = lock.createToken();
+        expect(token1.id).not.to.be.equal(token2.id);
+      });
 
-            var lock = new AsyncLock();
-
-            function updateBalance() {
-                lock.enter(function (token) {
-                    delay(balance.value, function (value) {
-                        value -= 10;
-                        delay(value, function (value) {
-                            balance.value = value;
-                            if (count === 0) {
-                                count++;
-                                lock.leave(token);
-                                return;
-                            }
-                            expect(balance.value).to.be.equal(80);
-                            done();
-                        });
-                    });
-                });
-            }
-
-
-            updateBalance();
-            updateBalance();
-
-        });
-
-
-
+      it('should execute a callback asynchronously', function (done) {
+        var lock = new AsyncLock();
+        var flag = true;
+        var callback = function () {
+          expect(flag).to.be.false;
+          done();
+        };
+        var token = lock.createToken(callback);
+        lock.executeCallback(token);
+        flag = false;
+      });
 
     });
+
+    describe('Enter a lock', function () {
+
+      it('should return a token', function (done) {
+        var lock = new AsyncLock();
+        var token = lock.enter(function (innerToken) {
+          expect(token.id).to.be.equal(innerToken.id);
+          done();
+        });
+      });
+
+      it('should execute the first entrant', function (done) {
+        var lock = new AsyncLock();
+        lock.enter(function () {
+          done();
+        });
+      });
+
+      it('should allow only one execution within a lock', function (done) {
+        var lock = new AsyncLock();
+        lock.enter(function () {
+          lock.enter(function () {
+            done('Should not be here');
+          });
+          done();
+        });
+      });
+
+      it('should not allow entering with a non function', function () {
+        var lock = new AsyncLock();
+        expect(function () {
+          lock.enter('hello world');
+        }).to.throw('Callback must be a function');
+      });
+
+      it('should throw if createToken returns null', function () {
+        var lock = new AsyncLock();
+        lock.createToken = function () { return null; };
+        expect(function () {
+          lock.enter(function () { });
+        }).to.throw('Token cannot be null or undefined');
+      });
+
+    });
+
+    describe('Leave a lock', function () {
+
+      it('should throw if token is null or undefined', function () {
+        var lock = new AsyncLock();
+        expect(function () {
+          lock.leave(null);
+        }).to.throw('Token cannot be null or undefined');
+
+        expect(function () {
+          lock.leave(undefined);
+        }).to.throw('Token cannot be null or undefined');
+
+      });
+
+      it('should throw if leave was called when there was no pending token', function () {
+        var lock = new AsyncLock();
+        expect(function () {
+          lock.leave('');
+        }).to.throw('There is no pending token in the lock but received ""');
+
+      });
+
+      it('should throw if leave was called with incorrect token', function (done) {
+        var lock = new AsyncLock();
+        lock.enter(function (token) {
+          token.id = 11;
+          expect(function () {
+            lock.leave(token);
+          }).to.throw('Owner token mismatch. Expected 0 but received 11');
+          done();
+        });
+      });
+
+      it('should allow enter after leave was called', function (done) {
+        var lock = new AsyncLock();
+        lock.enter(function (token) {
+          lock.leave(token);
+        });
+        lock.enter(function () {
+          done();
+        });
+      });
+
+      it('should execute the next entrant when leave was called', function (done) {
+        var lock = new AsyncLock();
+        lock.enter(function (token) {
+          setTimeout(function () {
+            lock.leave(token);
+          }, 100);
+        });
+        lock.enter(function () {
+          done();
+        });
+      });
+
+      it('should not execute the next entrant when leave was called if that token was canceled', function (done) {
+        var lock = new AsyncLock();
+        lock.enter(function (token) {
+          setTimeout(function () {
+            lock.leave(token);
+          }, 100);
+        });
+        var token = lock.enter(function () {
+          done('Should not be here');
+        });
+
+        token.isCanceled = true;
+
+        setTimeout(done, 300);
+      });
+    });
+
+    describe('Stop a lock', function () {
+
+      it('should throw if token is null or undefined', function () {
+        var lock = new AsyncLock();
+        expect(function () {
+          lock.stop(null);
+        }).to.throw('Token cannot be null or undefined');
+
+        expect(function () {
+          lock.stop(undefined);
+        }).to.throw('Token cannot be null or undefined');
+
+      });
+
+      it('should throw if stop was called when there was no pending token', function () {
+        var lock = new AsyncLock();
+        expect(function () {
+          lock.stop('');
+        }).to.throw('There is no pending token in the lock but received ""');
+
+      });
+
+      it('should throw if stop was called with incorrect token', function (done) {
+        var lock = new AsyncLock();
+        lock.enter(function (token) {
+          token.id = 11;
+          expect(function () {
+            lock.stop(token);
+          }).to.throw('Owner token mismatch. Expected 0 but received 11');
+          done();
+        });
+      });
+
+      it('should allow enter after stop was called', function (done) {
+        var lock = new AsyncLock();
+        lock.executeCallback = function (token) {
+          token.callback(token);
+        };
+        lock.enter(function (token) {
+          lock.stop(token);
+        });
+        lock.enter(function () {
+          done();
+        });
+      });
+
+
+      it('should not execute the next entrant when stop was called', function (done) {
+        var lock = new AsyncLock();
+        lock.enter(function (token) {
+          setTimeout(function () {
+            lock.stop(token);
+          }, 100);
+        });
+        lock.enter(function () {
+          done('Should not be here');
+        });
+
+        setTimeout(done, 300);
+      });
+    });
+  });
 });
